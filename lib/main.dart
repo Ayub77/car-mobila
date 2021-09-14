c354753cc140018458d94d61d56beb80157226f4
Ayubxon <nomonovmuhammadayubxon@gmail.com>
2021-09-14 13:36:54 +0500

    +

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_7/pages/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        HomePage.id: (context)=>HomePage(),
+      },
+    );
+  }
+}
+
+
